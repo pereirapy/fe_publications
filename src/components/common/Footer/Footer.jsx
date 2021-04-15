0ed07116584a6f811ac4dc0ae94a2aebf7de85e7
Rodrigo Pereira <pereirapy@hotmail.com>
2021-04-15 11:01:05 -0400

    improvement to the code

--- src/components/common/Footer/Footer.jsx
+++ src/components/common/Footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = (props) => {
   return (
     <Container fluid>
       <Row className="bg-light mt-4">
-        <Col className="mt-4 mb-4 text-center">{'message'}</Col>
+        <Col className="mt-4 mb-4 text-center">Created By Rodrigo Pereira</Col>
       </Row>
     </Container>
   )
